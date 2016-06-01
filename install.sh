c0a2c2e141fd9c06a162644ee9d9aaf0d8addb81
Tristan Pourcelot <tristan.pourcelot@ssi.gouv.fr>
2016-06-01 06:02:33 -0700

    Modify install.sh to follow readme

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #Prerequisites, on a ubuntu* distribution.
-sudo apt-get install -y git virtualenv ruby libffi-dev python-dev graphviz gcc libssl-dev python-pip
+#sudo apt-get install -y git virtualenv ruby libffi-dev python-dev graphviz gcc libssl-dev python-pip
 git submodule init metasm # or clone github.com/jjyg/metasm
 git submodule update metasm
 
@@ -19,8 +19,3 @@ pip install Flask-Misaka
 pip install Flask-Testing
 pip install graphviz
 pip install yara-python
-
-export PYTHONPATH=`pwd`:$PYTHONPATH
-./utils/db_create.py
-
-#./run.py
